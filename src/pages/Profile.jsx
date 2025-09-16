7b5ed409b29923460cc748056a687c7127bf53a4
damienbrozdziak <da.brozdziak@gmail.com>
2025-09-16 12:50:11 +0200

    Profile page: patch email,name,firstname

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { getUser } from "@/lib/utils";
 import { Check, Pencil } from "lucide-react";
 import { useState } from "react";
 import { userStore } from "./../lib/userStore";
-
+import { toast } from "sonner";
 export default function Profile() {
   const user = getUser();
 
@@ -13,6 +13,13 @@ export default function Profile() {
   const [emailModification, setEmailModification] = useState(false);
   const [email, setEmail] = useState(user ? user.email : "");
 
+  const [nameModification, setNameModification] = useState(false);
+  const [name, setName] = useState(user ? user.name : "");
+
+  const [firstnameModification, setFirstnameModification] = useState(false);
+  const [firstname, setFirstname] = useState(user ? user.firstname : "");
+
+  const emailRegex = /^\S+@\S+\.\S+$/;
   const handleClickInputEmail = () => {
     emailModification
       ? setEmailModification(false)
@@ -22,12 +29,48 @@ export default function Profile() {
     emailModification
       ? setEmailModification(false)
       : setEmailModification(true);
-
+    if (!emailRegex.test(email.trim())) {
+      setEmail(user.email);
+      return toast.error("Email invalide.");
+    }
     store.updateUser(user.id, {
       email: email,
     });
+    toast.success("Email modifié avec succès !");
   };
 
+  const handleClickInputName = () => {
+    nameModification ? setNameModification(false) : setNameModification(true);
+  };
+  const handleClickModifyName = () => {
+    nameModification ? setNameModification(false) : setNameModification(true);
+    if (name.trim().length === 0) {
+      setName(user.name);
+      return toast.error("Le nom est incorrect.");
+    }
+    store.updateUser(user.id, {
+      name: name,
+    });
+    toast.success("Nom modifié avec succès !");
+  };
+  const handleClickInputFirstname = () => {
+    firstnameModification
+      ? setFirstnameModification(false)
+      : setFirstnameModification(true);
+  };
+  const handleClickModifyFirstname = () => {
+    firstnameModification
+      ? setFirstnameModification(false)
+      : setFirstnameModification(true);
+    if (firstname.trim().length === 0) {
+      setFirstname(user.firstname);
+      return toast.error("Le prénom est incorrect.");
+    }
+    store.updateUser(user.id, {
+      firstname: firstname,
+    });
+    toast.success("Prénom modifié avec succès !");
+  };
   return (
     <>
       {user && (
@@ -56,17 +99,45 @@ export default function Profile() {
           </div>
           <div className="profile-item">
             <p>Nom: </p>
-            <span>{user.name}</span>{" "}
-            <div className="modify-pencil">
-              <Pencil />
-            </div>
+            {!nameModification ? (
+              <span>{name}</span>
+            ) : (
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            )}{" "}
+            {!nameModification ? (
+              <div className="modify-pencil">
+                <Pencil onClick={handleClickInputName} />
+              </div>
+            ) : (
+              <div className="modify-check">
+                <Check onClick={handleClickModifyName} />
+              </div>
+            )}
           </div>
           <div className="profile-item">
             <p>Prénom: </p>
-            <span>{user.firstname} </span>
-            <div className="modify-pencil">
-              <Pencil />
-            </div>
+            {!firstnameModification ? (
+              <span>{firstname}</span>
+            ) : (
+              <input
+                type="text"
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+            )}{" "}
+            {!firstnameModification ? (
+              <div className="modify-pencil">
+                <Pencil onClick={handleClickInputFirstname} />
+              </div>
+            ) : (
+              <div className="modify-check">
+                <Check onClick={handleClickModifyFirstname} />
+              </div>
+            )}
           </div>
         </div>
       )}
@@ -78,4 +149,3 @@ export default function Profile() {
     </>
   );
 }
-git
